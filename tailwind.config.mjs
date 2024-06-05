0d8440e4f1fca73127dc7b17fa0ef9468ba80d11
speedyibbi <ibbi2k2.khan@gmail.com>
2024-06-06 00:47:58 +0500

    perf: :zap: add some content to index file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				accent: 'var(--accent)',
+			},
+			fontFamily: {
+				'bebas-neue': ['Bebas Neue', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
