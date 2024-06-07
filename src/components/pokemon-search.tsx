b3c6cdffeea78c5d02040a43907953d0c218a964
speedyibbi <ibbi2k2.khan@gmail.com>
2024-06-08 04:18:15 +0500

    perf: :zap: add search functionality in pokemon search modal

--- src/components/pokemon-search.tsx
+++ src/components/pokemon-search.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useStore } from '@nanostores/react';
 import { $modalState } from '@/stores/modal';
 import type { pokemon } from '@/stores/team';
@@ -10,24 +11,30 @@ interface Props {
 
 export default function PokemonSearch({ pokemon }: Props) {
 	const modalState = useStore($modalState);
+	const [searchedPokemon, setSearchedPokemon] = useState(pokemon);
 
-	const submissionHandler = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+	const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchedPokemon(
+			pokemon.filter((pokemon) => pokemon.name.includes(event.target.value))
+		);
 	};
 
 	return modalState === 'search' ? (
 		<dialog className='w-screen h-screen p-[3.125vw_6.25vw] inset-0 flex flex-col place-content-start place-items-center gap-[5.2vw] bg-black/50 backdrop-blur-3xl'>
 			<form
-				onSubmit={submissionHandler}
+				onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+					event.preventDefault();
+				}}
 				className='w-[45vw] p-[0.75vw] border-b-[0.1vw] border-foreground'
 			>
 				<input
+					onChange={searchHandler}
 					placeholder='Search'
 					className='w-full font-bebas-neue text-[1.25vw] text-foreground placeholder:text-foreground text-center tracking-[-3%] leading-[100%] bg-transparent outline-none'
 				/>
 			</form>
-			<div className='w-full flex flex-wrap place-content-start place-items-end overflow-y-scroll'>
-				{pokemon.map((pokemon, idx) => (
+			<div className='w-full relative flex flex-wrap place-content-start place-items-end overflow-y-scroll'>
+				{searchedPokemon.map((pokemon, idx) => (
 					<img
 						key={idx}
 						src={pokemon.sprite}
