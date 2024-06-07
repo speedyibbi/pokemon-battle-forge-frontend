998a3519fe82341860e3ac42d6166415114aa3af
speedyibbi <ibbi2k2.khan@gmail.com>
2024-06-07 17:21:29 +0500

    refactor: :recycle: refactor back button into separate component

---
+++ src/components/back-button.tsx
@@ -0,0 +1,30 @@
+import { $modalState } from '@/stores/modal';
+
+export default function BackButton() {
+	return (
+		<button onClick={() => $modalState.set('closed')}>
+			<svg
+				viewBox='0 0 48 48'
+				fill='none'
+				stroke-width='0'
+				className='w-[2.5vw] fill-foreground stroke-background hover:scale-[1.2] transition-transform duration-150 will-change-transform'
+			>
+				<g clip-path='url(#clip0_166_189)'>
+					<circle cx='24' cy='24' r='24' fill='inherit'></circle>
+					<path
+						d='M17.3334 24.0001H30.6667M17.3334 24.0001L22.6667 18.6667M17.3334 24.0001L22.6667 29.3334'
+						stroke='inherit'
+						stroke-width='2'
+						stroke-linecap='round'
+						stroke-linejoin='round'
+					></path>
+				</g>
+				<defs>
+					<clipPath id='clip0_166_189'>
+						<rect width='48' height='48' fill='inherit'></rect>
+					</clipPath>
+				</defs>
+			</svg>
+		</button>
+	);
+}
--- src/components/side-buttons.tsx
+++ src/components/side-buttons.tsx
@@ -80,30 +80,6 @@ export default function SideButtons() {
 						</defs>
 					</svg>
 				</button>
-				<button>
-					<svg
-						viewBox='0 0 48 48'
-						fill='none'
-						stroke-width='0'
-						className='w-[2.5vw] fill-foreground stroke-background hover:scale-[1.2] transition-transform duration-150 will-change-transform'
-					>
-						<g clip-path='url(#clip0_166_189)'>
-							<circle cx='24' cy='24' r='24' fill='inherit'></circle>
-							<path
-								d='M17.3334 24.0001H30.6667M17.3334 24.0001L22.6667 18.6667M17.3334 24.0001L22.6667 29.3334'
-								stroke='inherit'
-								stroke-width='2'
-								stroke-linecap='round'
-								stroke-linejoin='round'
-							></path>
-						</g>
-						<defs>
-							<clipPath id='clip0_166_189'>
-								<rect width='48' height='48' fill='inherit'></rect>
-							</clipPath>
-						</defs>
-					</svg>
-				</button>
 			</span>
 		</aside>
 	);
