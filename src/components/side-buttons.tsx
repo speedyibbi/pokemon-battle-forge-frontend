0c35cf58d717c14bbaa423d9bd27182ec476af9a
speedyibbi <ibbi2k2.khan@gmail.com>
2024-06-08 16:20:39 +0500

    fix: :bug: adjust side buttons attributes

--- src/components/side-buttons.tsx
+++ src/components/side-buttons.tsx
@@ -12,7 +12,7 @@ export default function SideButtons() {
 						<svg
 							viewBox='0 0 48 48'
 							fill='none'
-							stroke-width='0'
+							strokeWidth='0'
 							className='w-[2.5vw] fill-foreground stroke-background hover:scale-[1.2] transition-transform duration-150 will-change-transform'
 						>
 							<g clip-path='url(#clip0_151_201)'>
@@ -20,9 +20,9 @@ export default function SideButtons() {
 								<path
 									d='M31.25 17.25L17.25 31.25M17.25 17.25L31.25 31.25'
 									stroke='inherit'
-									stroke-width='2'
-									stroke-linecap='round'
-									stroke-linejoin='round'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'
 								></path>
 							</g>
 							<defs>
@@ -37,7 +37,7 @@ export default function SideButtons() {
 					<svg
 						viewBox='0 0 48 48'
 						fill='none'
-						stroke-width='0'
+						strokeWidth='0'
 						className='w-[2.5vw] fill-foreground stroke-background hover:scale-[1.2] transition-transform duration-150 will-change-transform'
 					>
 						<g clip-path='url(#clip0_151_170)'>
@@ -45,8 +45,8 @@ export default function SideButtons() {
 							<path
 								d='M13.3334 14.6667H21.3334M21.3334 14.6667C21.3334 16.1394 22.5272 17.3333 24 17.3333C25.4728 17.3333 26.6667 16.1394 26.6667 14.6667M21.3334 14.6667C21.3334 13.1939 22.5272 12 24 12C25.4728 12 26.6667 13.1939 26.6667 14.6667M26.6667 14.6667H34.6667M13.3334 24H29.3334M29.3334 24C29.3334 25.4728 30.5272 26.6667 32 26.6667C33.4728 26.6667 34.6667 25.4728 34.6667 24C34.6667 22.5272 33.4728 21.3333 32 21.3333C30.5272 21.3333 29.3334 22.5272 29.3334 24ZM18.6667 33.3333H34.6667M18.6667 33.3333C18.6667 31.8605 17.4728 30.6667 16 30.6667C14.5273 30.6667 13.3334 31.8605 13.3334 33.3333C13.3334 34.8061 14.5273 36 16 36C17.4728 36 18.6667 34.8061 18.6667 33.3333Z'
 								stroke='inherit'
-								stroke-width='2'
-								stroke-linecap='round'
+								strokeWidth='2'
+								strokeLinecap='round'
 							></path>
 						</g>
 						<defs>
@@ -60,7 +60,7 @@ export default function SideButtons() {
 					<svg
 						viewBox='0 0 48 48'
 						fill='none'
-						stroke-width='0'
+						strokeWidth='0'
 						className='w-[2.5vw] fill-foreground stroke-background hover:scale-[1.2] transition-transform duration-150 will-change-transform'
 					>
 						<g clip-path='url(#clip0_149_160)'>
@@ -68,9 +68,9 @@ export default function SideButtons() {
 							<path
 								d='M32.9639 21V27M32.9639 27H26.9639M32.9639 27C30.6365 24.9114 28.481 22.5468 25.2186 22.0878C23.2862 21.8159 21.3179 22.1794 19.6101 23.1234C17.9022 24.0675 16.5475 25.5409 15.75 27.3218'
 								stroke='inherit'
-								stroke-width='2'
-								stroke-linecap='round'
-								stroke-linejoin='round'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
 							></path>
 						</g>
 						<defs>
